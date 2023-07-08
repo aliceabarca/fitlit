9474c8f609b9fc3fbf169248662632822946c6fe
Alice Abarca <aliceabarca@otmail.com>
2023-07-08 17:59:01 -0600

    Get average of all step goals

--- src/model.js
+++ src/model.js
@@ -1,3 +1,9 @@
 export function getUserData(users, id) {
   return users.find(user => user.id === id);
 }
+
+export function getAllAvgSteps(users) {
+  return (
+    users.reduce((acc, user) => (acc += user.dailyStepGoal), 0) / users.length
+  );
+}
