9bbb025aafbbb9ac1a8707b58d094352dcea400b
Alice Abarca <aliceabarca@otmail.com>
2023-07-08 17:39:02 -0600

    Return user data

--- src/scripts.js
+++ src/scripts.js
@@ -10,10 +10,3 @@ import './images/turing-logo.png';
 
 // An example of how you tell webpack to use a JS file
 import userData from './data/users';
-console.log("User Data:", userData);
-
-// Example of one way to import functions from the domUpdates file.  You will delete these examples.
-import { exampleFunction1, exampleFunction2 } from './domUpdates';
-
-exampleFunction1('Travis');
-exampleFunction2('Travis')
