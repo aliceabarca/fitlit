23c262f06d0f0e8d07bce67c9aa47ee98e1b7ad8
Renee Pinna <renee.pinna@gmail.com>
2023-07-08 20:57:16 -0600

    Add function to retrive users all time water intake average

--- src/model.js
+++ src/model.js
@@ -1,3 +1,8 @@
 export function getUserData(users, id) {
   return users.find(user => user.id === id);
 }
+
+export function getAverageWater(hydrationData, id) {
+  const userData = hydrationData.filter(data => data.userID === id); 
+  return userData.reduce((sum, date) => sum + date.numOunces, 0) / userData.length;
+}
