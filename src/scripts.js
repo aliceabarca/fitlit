c56a92ae9bc5335e599f8a1e4d4ce6ee479ab62c
Renee Pinna <renee.pinna@gmail.com>
2023-07-11 15:43:55 -0600

    Add network request

--- src/scripts.js
+++ src/scripts.js
@@ -5,23 +5,37 @@ import { displayUsersName, showWeeklyWaterIntake } from './domUpdates';
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 import { showUserData, showUserStepsVsAvg, showCurrentDayWaterIntake } from './domUpdates';
-import { getRandomUser, getUserStepGoal, getAllAvgSteps, getUserHydrationData } from './model';
+import { getRandomUser, getUserStepGoal, getAllAvgSteps, getUserHydrationData, getUserDataFromAPI, getApiData} from './model';
 import userData from './data/users';
 import hydrationData from './data/hydration';
 
-const store = {
-  userData: userData.users,
+export const store = {
+  userData: [],
   hydrationData: hydrationData.hydrationData,
   user: getRandomUser(userData.users),
 };
 
 window.onload = () => {
-  const userSteps = store.user.dailyStepGoal;
-  const avg = getAllAvgSteps(store.userData);
-  const userHydrationData = getUserHydrationData(store.hydrationData, store.user.id)
-  showCurrentDayWaterIntake(userHydrationData, '2023/03/31');
-  showUserData(store.user);
-  showUserStepsVsAvg(userSteps, avg);
-  displayUsersName(store.user);
-  showWeeklyWaterIntake(userHydrationData);
+  Promise.all([
+    getApiData('https://fitlit-api.herokuapp.com/api/v1/users'),
+    getApiData('https://fitlit-api.herokuapp.com/api/v1/sleep'),
+    getApiData('https://fitlit-api.herokuapp.com/api/v1/hydration'),
+    getApiData('https://fitlit-api.herokuapp.com/api/v1/activity'),
+  ])
+    .then( prom => {
+      console.log(prom)
+      store.userData = prom[0];
+      store.hydrationData = prom[1]
+    })
+    .then( notProm => {
+      console.log(store.userData)
+      const userSteps = store.user.dailyStepGoal;
+      const avg = getAllAvgSteps(store.userData);
+      const userHydrationData = getUserHydrationData(store.hydrationData, store.user.id)
+      showCurrentDayWaterIntake(userHydrationData, '2023/03/31');
+      showUserData(store.user);
+      showUserStepsVsAvg(userSteps, avg);
+      displayUsersName(store.user);
+      showWeeklyWaterIntake(userHydrationData);
+    })
 };
