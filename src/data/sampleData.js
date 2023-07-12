638e11ad789b8332e70e69b933f5ebec4d1de27d
Josh Bennett <joshbennett793@yahoo.com>
2023-07-11 20:36:04 -0500

    Delete local data files

--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -30,56 +30,54 @@ export default {
   ],
   hydration: [
     {
-      "userID": 1,
-      "date": "2023/03/23",
-      "numOunces": 28
+      userID: 1,
+      date: '2023/03/23',
+      numOunces: 28,
     },
     {
-      "userID": 2,
-      "date": "2023/03/24",
-      "numOunces": 35
+      userID: 2,
+      date: '2023/03/24',
+      numOunces: 35,
     },
     {
-      "userID": 1,
-      "date": "2023/03/24",
-      "numOunces": 50
+      userID: 1,
+      date: '2023/03/24',
+      numOunces: 50,
     },
     {
-      "userID": 1,
-      "date": "2023/03/26",
-      "numOunces": 21
+      userID: 1,
+      date: '2023/03/26',
+      numOunces: 21,
     },
     {
-      "userID": 2,
-      "date": "2023/03/26",
-      "numOunces": 88
+      userID: 2,
+      date: '2023/03/26',
+      numOunces: 88,
     },
     {
-      "userID": 1,
-      "date": "2023/03/27",
-      "numOunces": 22
+      userID: 1,
+      date: '2023/03/27',
+      numOunces: 22,
     },
     {
-      "userID": 1,
-      "date": "2023/03/28",
-      "numOunces": 38
+      userID: 1,
+      date: '2023/03/28',
+      numOunces: 38,
     },
     {
-      "userID": 1,
-      "date": "2023/03/29",
-      "numOunces": 96
+      userID: 1,
+      date: '2023/03/29',
+      numOunces: 96,
     },
     {
-      "userID": 1,
-      "date": "2023/03/30",
-      "numOunces": 20
+      userID: 1,
+      date: '2023/03/30',
+      numOunces: 20,
     },
     {
-      "userID": 1,
-      "date": "2023/03/31",
-      "numOunces": 86
+      userID: 1,
+      date: '2023/03/31',
+      numOunces: 86,
     },
-
-
-  ]
+  ],
 };
