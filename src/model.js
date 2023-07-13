0cf14eef9d2e4d8d0abe2da0e5e54cf5f42fcd73
Alice Abarca <aliceabarca@otmail.com>
2023-07-13 10:59:57 -0600

    Edit the function get user data

--- src/model.js
+++ src/model.js
@@ -1,5 +1,12 @@
-export function getUserData(users, id) {
-  return users.find(user => user.id === id);
+export function getUserData(dataType, users, id) {
+  switch (dataType) {
+    case 'hydrationData':
+      return data.filter(data => data.userID === id);
+    case 'sleepData':
+      return data.filter(data => data.userID === id);
+    default:
+      return data.find(data => data.id === id);
+  }
 }
 
 function getRandomID(array) {
@@ -26,12 +33,12 @@ export function getAverageWater(userData) {
   }
 
   return Math.round(
-    userData.reduce((sum, date) => sum + date.numOunces, 0) / userData.length,
+    userData.reduce((sum, date) => sum + date.numOunces, 0) / userData.length
   );
 }
 
 export function getDailyWater(userHydrationData, date) {
-  const userData = userHydrationData.find(data => data.date === date);
+  const userData = userHydrationData.find((data) => data.date === date);
 
   if (!userData) {
     return 0;
@@ -41,7 +48,7 @@ export function getDailyWater(userHydrationData, date) {
 }
 
 export function getUserHydrationData(hydrationData, id) {
-  return hydrationData.filter(data => data.userID === id);
+  return hydrationData.filter((data) => data.userID === id);
 }
 
 export function getWeeklyWater(userData) {
@@ -58,21 +65,24 @@ export function getWeeklyWater(userData) {
 }
 
 export function getUserSleepData(sleepData, id) {
-  return sleepData.filter(data => data.userID === id)
-  }
+  return sleepData.filter((data) => data.userID === id);
+}
 
 export function getAvgSleepPerDay(userData) {
-    const avg = userData.reduce((acc, userData) => (acc += userData.hoursSlept), 0) / userData.length
-
-    if (!userData.length) {
-      return 0;
-    } 
-
-    return avg;
+  if (!userData.length) {
+    return 0;
   }
+
+  const avg =
+    userData.reduce((acc, userData) => (acc += userData.hoursSlept), 0) /
+    userData.length;
+
+  return avg;
+}
 
 export function getAllAvgSleep(userData) {
   return (
-    userData.reduce((acc, user) => (acc += user.sleepQuality), 0) / userData.length
+    userData.reduce((acc, user) => (acc += user.sleepQuality), 0) /
+    userData.length
   );
 }
