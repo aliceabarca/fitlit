d93a3aaaf2a75322f4a6bd7ceaf0d85c09b09b83
Jen Nguyen <jenniferannnguyen@live.com>
2023-07-13 22:30:33 -0600

    Modify the sample data for sleep quality on user 3 to be 5 or less

--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -2,27 +2,27 @@ export default {
   users: [
     {
       id: 1,
-      name: 'Trystan Gorczany',
-      address: '9484 Lucas Flat, West Kittymouth WA 67504',
-      email: 'Taurean_Pollich31@gmail.com',
+      name: "Trystan Gorczany",
+      address: "9484 Lucas Flat, West Kittymouth WA 67504",
+      email: "Taurean_Pollich31@gmail.com",
       strideLength: 4,
       dailyStepGoal: 6000,
       friends: [5, 43, 46, 11],
     },
     {
       id: 2,
-      name: 'Tyreek VonRueden',
-      address: '623 Koelpin Skyway, Lake Luigichester MN 77576-1678',
-      email: 'Nicolette_Halvorson43@yahoo.com',
+      name: "Tyreek VonRueden",
+      address: "623 Koelpin Skyway, Lake Luigichester MN 77576-1678",
+      email: "Nicolette_Halvorson43@yahoo.com",
       strideLength: 4.5,
       dailyStepGoal: 9000,
       friends: [13, 19, 3],
     },
     {
       id: 3,
-      name: 'Colt Rohan',
-      address: '48010 Balistreri Harbor, Cleobury IN 43317',
-      email: 'Wilford.Barton@gmail.com',
+      name: "Colt Rohan",
+      address: "48010 Balistreri Harbor, Cleobury IN 43317",
+      email: "Wilford.Barton@gmail.com",
       strideLength: 2.7,
       dailyStepGoal: 3000,
       friends: [31, 16, 15, 7],
@@ -31,120 +31,118 @@ export default {
   hydration: [
     {
       userID: 1,
-      date: '2023/03/23',
+      date: "2023/03/23",
       numOunces: 28,
     },
     {
       userID: 2,
-      date: '2023/03/24',
+      date: "2023/03/24",
       numOunces: 35,
     },
     {
       userID: 1,
-      date: '2023/03/24',
+      date: "2023/03/24",
       numOunces: 50,
     },
     {
       userID: 1,
-      date: '2023/03/26',
+      date: "2023/03/26",
       numOunces: 21,
     },
     {
       userID: 2,
-      date: '2023/03/26',
+      date: "2023/03/26",
       numOunces: 88,
     },
     {
       userID: 1,
-      date: '2023/03/27',
+      date: "2023/03/27",
       numOunces: 22,
     },
     {
       userID: 1,
-      date: '2023/03/28',
+      date: "2023/03/28",
       numOunces: 38,
     },
     {
       userID: 1,
-      date: '2023/03/29',
+      date: "2023/03/29",
       numOunces: 96,
     },
     {
       userID: 1,
-      date: '2023/03/30',
+      date: "2023/03/30",
       numOunces: 20,
     },
     {
       userID: 1,
-      date: '2023/03/31',
+      date: "2023/03/31",
       numOunces: 86,
     },
     {
       userID: 3,
-      date: '2023/03/31',
+      date: "2023/03/31",
       numOunces: 86,
-    }
-  ],
-  sleepData:
-    [
-      {
-        userID:1,
-        date: '2023/03/24',
-        hoursSlept: 9.6,
-        sleepQuality:4.3
-      },
-      {
-        userID:2,
-        date: '2023/03/25',
-        hoursSlept: 8.4,
-        sleepQuality: 3.5
-      },
-      {
-        userID:3,
-        date: '2023/03/24',
-        hoursSlept: 8.3,
-        sleepQuality: 3.5
-      },
-      {
-        userID:3,
-        date: '2023/03/25',
-        hoursSlept: 8.0,
-        sleepQuality: 4.5
-      },
-      {
-        userID:3,
-        date: '2023/03/26',
-        hoursSlept: 8.1,
-        sleepQuality: 3.5
-      },
-      {
-        userID:3,
-        date: '2023/03/27',
-        hoursSlept: 7.5,
-        sleepQuality: 4.3
-      },
-      {
-        userID:3,
-        date: '2023/03/28',
-        hoursSlept: 7.5,
-        sleepQuality: 2.5
-      },
-      {
-        userID:3,
-        date: '2023/03/29',
-        hoursSlept: 7.1,
-        sleepQuality: 5.2
-      },
-      {
-        userID:3,
-        date: '2023/03/30',
-        hoursSlept: 7.5,
-        sleepQuality: 3.7
-      },
-      { userID:3,
-      date: '2023/03/31',
-      hoursSlept: 6.9,
-      sleepQuality: 3.5
     },
-    ]
+  ],
+  sleepData: [
+    {
+      userID: 1,
+      date: "2023/03/24",
+      hoursSlept: 9.6,
+      sleepQuality: 4.3,
+    },
+    {
+      userID: 2,
+      date: "2023/03/25",
+      hoursSlept: 8.4,
+      sleepQuality: 3.5,
+    },
+    {
+      userID: 3,
+      date: "2023/03/24",
+      hoursSlept: 8.3,
+      sleepQuality: 3.5,
+    },
+    {
+      userID: 3,
+      date: "2023/03/25",
+      hoursSlept: 8.0,
+      sleepQuality: 4.5,
+    },
+    {
+      userID: 3,
+      date: "2023/03/26",
+      hoursSlept: 8.1,
+      sleepQuality: 3.5,
+    },
+    {
+      userID: 3,
+      date: "2023/03/27",
+      hoursSlept: 7.5,
+      sleepQuality: 4.3,
+    },
+    {
+      userID: 3,
+      date: "2023/03/28",
+      hoursSlept: 7.5,
+      sleepQuality: 2.5,
+    },
+    {
+      userID: 3,
+      date: "2023/03/29",
+      hoursSlept: 7.1,
+      sleepQuality: 5,
+    },
+    {
+      userID: 3,
+      date: "2023/03/30",
+      hoursSlept: 7.5,
+      sleepQuality: 3.7,
+    },
+    { userID: 3, 
+      date: "2023/03/31", 
+      hoursSlept: 6.9, 
+      sleepQuality: 3.5 },
+  ],
 };
