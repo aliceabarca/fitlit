18bc5a8e21dac607eb5c6051b181ce30193350cb
Josh Bennett <joshbennett793@yahoo.com>
2023-07-13 22:15:30 -0500

    Keep variable store private within function closure

--- src/scripts.js
+++ src/scripts.js
@@ -7,46 +7,75 @@ import {
   showUserStepsVsAvg,
   showCurrentDayWaterIntake,
 } from './domUpdates';
-import { getRandomUser, getAllAvgSteps, getDailyWater, getUserData, getCurrentWaterDate} from './model';
+import {
+  getRandomUser,
+  getAllAvgSteps,
+  getDailyWater,
+  getUserData,
+  getCurrentWaterDate,
+} from './model';
 import { getApiData } from './apiCalls';
 
-const store = {
-  apiKeys: {
+function initializeStore() {
+  const store = {
     users: 'https://fitlit-api.herokuapp.com/api/v1/users',
     sleep: 'https://fitlit-api.herokuapp.com/api/v1/sleep',
     hydration: 'https://fitlit-api.herokuapp.com/api/v1/hydration',
     activity: 'https://fitlit-api.herokuapp.com/api/v1/activity',
-  },
-};
+  };
 
-window.onload = initializeApp;
+  return {
+    getAPIKey(endpoint) {
+      return store[endpoint];
+    },
+
+    getKey(key) {
+      return store[key];
+    },
+
+    setKey(key, value) {
+      store[key] = value;
+    },
+  };
+}
+
+let store;
+
+window.onload = () => {
+  store = initializeStore();
+  initializeApp();
+};
 
 function initializeApp() {
   Promise.all([
-    getApiData(store.apiKeys.users, 'users'),
-    getApiData(store.apiKeys.sleep, 'sleepData'),
-    getApiData(store.apiKeys.hydration, 'hydrationData'),
-    getApiData(store.apiKeys.activity, 'activityData'),
+    getApiData(store.getAPIKey('users'), 'users'),
+    getApiData(store.getAPIKey('sleep'), 'sleepData'),
+    getApiData(store.getAPIKey('hydration'), 'hydrationData'),
+    getApiData(store.getAPIKey('activity'), 'activityData'),
   ])
     .then(values => {
       const [users, sleepData, hydrationData, activityData] = values;
-      store.userData = users;
-      store.sleepData = sleepData;
-      store.hydrationData = hydrationData;
-      store.activityData = activityData;
+      store.setKey('userData', users);
+      store.setKey('sleepData', sleepData);
+      store.setKey('hydrationData', hydrationData);
+      store.setKey('activityData', activityData);
     })
     .then(processUserData);
 }
 
 function processUserData() {
-  store.user = getRandomUser(store.userData);
-  const userSteps = store.user.dailyStepGoal;
-  const avg = getAllAvgSteps(store.userData);
-  const userHydrationData = getUserData('hydrationData', store.hydrationData, store.user.id);
-  const todaysWaterIntake = getDailyWater(userHydrationData, getCurrentWaterDate(userHydrationData));
-  showCurrentDayWaterIntake(todaysWaterIntake);
-  showUserData(store.user);
+  const userData = store.getKey('userData');
+  store.setKey('user', getRandomUser(userData));
+  const userSteps = store.getKey('user').dailyStepGoal;
+  const avg = getAllAvgSteps(userData);
+  const userHydrationData = getUserData(
+    'hydrationData',
+    store.getKey('hydrationData'),
+    store.getKey('user').id,
+  );
+  showCurrentDayWaterIntake(getDailyWater(userHydrationData, '2023/03/31'));
+  showUserData(store.getKey('user'));
   showUserStepsVsAvg(userSteps, avg);
-  displayUsersName(store.user);
+  displayUsersName(store.getKey('user'));
   showWeeklyWaterIntake(userHydrationData);
 }
